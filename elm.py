5e2911dfcad80d306f1e90bd032b52df1f6e01a7
alyonarodin <alyona.rodin@gmail.com>
2018-07-31 18:56:56 -0400

    added

--- elm.py
+++ elm.py
@@ -16,6 +16,73 @@ from nltk.tokenize import word_tokenize
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
+#start and end dates of time period
+start = datetime(2012,11,20)
+end = datetime(2017,9,29)
+# Tesla ticker
+ticker='TSLA'
+#we read the data of ticker from yahoo/finance
+data=web.DataReader(ticker, 'yahoo', start, end)
+#print(data)
+
+#the data can be downloaded from the csv. Since we will somewhat use the dates I 
+#used csv to get dates as the column
+data=pd.read_csv(r'/Users/alyonarodin/Desktop/python_em_tweets/TSLA.csv')
+#percent change calculated using Adj Close (the Close prices after amendments)
+percent_change=data['Adj Close'].pct_change()*100
+volume=data['Volume']
+#we took two columns of the data downloaded from the yahoo website
+#print(percent_change[1:])
+#print(volume[1:])
+
+#calculated the column(variable) of direction of the return Up and Down
+list_up_down=[]
+n=0
+for value in percent_change:
+        #print(value)
+        if (value>0.0):
+           list_up_down.append('Up')
+           n+=1
+        else:
+           list_up_down.append('Down')
+           n+=1
+
+#print(list_up_down) 
+#print(data['Date'][1:])  
+#created a table : Date, Adj Close, Return, Volume, Direction. Adj Close and
+#Return will be highly correlated, but included... 
+stock_data=pd.DataFrame(list(zip(data['Date'][1:],data['Adj Close'][1:],percent_change[1:],volume[1:],list_up_down)), columns=['date','AdjClose','percentage_change', 'volumne', 'Direction'])
+#print('Maximum percent change: ', format(percent_change.max()*100, '.2f'),'%')
+#print(stock_data)
+
+#transfered to .json format to be able to use it in the Tableau
+stock_Data_json=stock_data.to_json(orient='table')
+outfile = open("/Users/alyonarodin/Desktop/python_em_tweets/stock_Data_json.json", "w")
+outfile.write(stock_Data_json)
+outfile.close()
+
+#but we plotted it also in Plotly. First is a Adj close prices and second data is return
+#define the data
+trace_adjClose=go.Scatter(
+        x=stock_data['date'],
+        y=stock_data['AdjClose'],
+        name='Adj Close',
+        line=dict(width=1,color='#17BECF'),
+        opacity=0.9 
+        
+        )
+trace_perc_change=go.Scatter(
+        x=stock_data['date'],
+        y=stock_data['percentage_change'],
+        name='Return',
+        line=dict(width=1,color='#FF5733'),
+        opacity=0.9
+        )
+data=[trace_adjClose, trace_perc_change ]
+
+#plotting 
+fig=(data)
+py.plot(fig,filename='Adj Close prices of Tesla and Return')
 #downloaded data
 data_elonmusk=pd.read_csv('/users/alyonarodin/Desktop/elonmusk.csv', encoding ='latin1')
 #print(data_elonmusk.head())
@@ -68,20 +135,60 @@ list_pos_neg=[]
 for value in dict_tweets:
      list_pos_neg.append(dict_tweets[value])
 #print(list_pos_neg[1:4])
-
-
-#created table that contatins tweets, date, and if the sentence positive/negative
+#create table with Tweets, Time, and pos/neg/neu
 DF_list_tweets=pd.DataFrame(list(zip(data_elonmusk['Tweet'], data_elonmusk['Time'],list_pos_neg)), columns=['tweet','time', 'pos/neg/neu'])
 #print(DF_list_tweets)
+#prepared only the date (without time)
+#print(d.head())
 
-#converted the table to json file to plot in tableau
-tweet_json=DF_list_tweets.to_json(orient='table')
-print(tweet_json)
-outfile = open("/Users/alyonarodin/Desktop/plotly_em/twitter_neg_pos_json.json", "w")
+list_time=[t.split(' ')[0] for t in data_elonmusk['Time'] ]
+#print(list_time[:10])
 
-outfile.write(tweet_json)
+#transfered to Dataframe the time
+DF_time_column=pd.DataFrame(list_time)
+#print(DF_time_column)
+#calculated the number tweets per each day
+new_list_num_tweets=[list_time.count(str(date)) for date in list_time]
+     
+#print(new_list_num_tweets)
+DF_list_tweets=pd.DataFrame(list(zip(data_elonmusk['Tweet'], data_elonmusk['Time'],new_list_num_tweets,list_pos_neg)), columns=['tweet','time','#tweets','pos/neg/neu'])
+#print(DF_list_tweets)
+
+#transfered this to json 
+number_tweetsPerDay_json=DF_list_tweets.to_json(orient='table')
+#print(number_tweetsPerDay_json)
+outfile = open("/Users/alyonarodin/Desktop/python_em_tweets/elm_number_tweetsPerDay.json", "w")
+
+outfile.write(number_tweetsPerDay_json)
 outfile.close()
 
+#print(DF_list_tweets)
+#print(stock_data)
+#reversed the data
+new_list_pos_neg=list(reversed(list_pos_neg))
+new_list_data_tweets=list(reversed(data_elonmusk['Tweet']))
+new_list_num_tweets=list(reversed(new_list_num_tweets))
+new_list_time=list(reversed(list_time))
+#created new table
+DF_list_tweets=pd.DataFrame(list(zip(new_list_pos_neg,new_list_data_tweets,new_list_num_tweets,new_list_time)), columns=['ps/neg/neu','tweets','#tweets','time'])
+print(DF_list_tweets)
+
+#to count if rt or not (1,0)
+rt=['1' if 'rt' in i else '0' for i in DF_list_tweets['tweets']]
+#print(rt)
+
+#if a tesla in tweet 1 if not 0
+tesla_word=['1' if 'tesla' in i else '0' for i in DF_list_tweets['tweets']]
+#print(tesla_word)
+#if a spaceX in tweet 1 if not 0
+spaceX=['1' if 'spacex' in i else '0' for i in DF_list_tweets['tweets']]
+#print(spaceX)
+#if a model in tweet 1 if not 0
+model=['1' if 'model' in i else '0' for i in DF_list_tweets['tweets']]
+#print(model)
+
+DF_list_tweets_new_table=pd.DataFrame(list(zip(new_list_pos_neg,new_list_data_tweets,new_list_num_tweets,new_list_time,rt,tesla_word,spaceX,model)), columns=['ps/neg/neu','tweets','#tweets','time','rt','tesla','spacex','model'])
+print( DF_list_tweets_new_table)
 
 
 #the most common words
@@ -230,6 +337,12 @@ twitter_data=DF_list_words_new.to_json(orient='table')
 outfile=open("/Users/alyonarodin/Desktop/plotly_em/twitter_data.json", "w")
 outfile.write(twitter_data)
 outfile.close()
+
+
+
+
+
+
 
 #second Plot made in plotly
 
@@ -402,4 +515,48 @@ trace3 = go.Scatter(
 )
 data = [trace1, trace2, trace3]
 py.iplot(data, filename='')
+
+tw=list(DF_list_tweets_new_table['time'])
+s=list(stock_data['date'])
+#created 4 lists to append 
+list_adjCl=[]
+list_return=[]
+list_volume=[]
+list_direction=[]
+#appended list with new columns, based on the date- to match
+for i in tw:
+    if i in s:
+        ind=s.index(i)
+        list_adjCl.append(stock_data['AdjClose'][ind])
+        list_return.append(stock_data['percentage_change'][ind])
+        list_volume.append(stock_data['volumne'][ind])
+        list_direction.append(stock_data['Direction'][ind])
+    else:
+        list_adjCl.append('NaN')    
+        list_return.append('NaN') 
+        list_volume.append('NaN') 
+        list_direction.append('NaN') 
+#created new table
+DF_list_tweets_new_table=pd.DataFrame(list(zip(new_list_pos_neg,new_list_time,rt,tesla_word,spaceX,model,list_adjCl,list_return,list_volume,list_direction)), columns=['ps/neg/neu','time','rt','tesla','spacex','model','list_adjCl','list_return','list_volume','list_direction'])
+print(DF_list_tweets_new_table)
+#since we have values NAN, we delete them. As there are few tweets per day 
+#se assigned the values from table stock_data to those tweets if the dates match. 
+d=DF_list_tweets_new_table[DF_list_tweets_new_table.list_adjCl!= 'NaN']
+#print(len(d))
+
+#next we perform analyses for the table just has been created
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
