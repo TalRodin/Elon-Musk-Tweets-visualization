1e50c12c185b7ee5068e55a6da462408cbc9cca1
alyonarodin <alyona.rodin@gmail.com>
2018-08-02 19:17:26 -0400

    added

--- elm.py
+++ elm.py
@@ -1,9 +1,9 @@
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+from plotly.offline import  plot
 import plotly.graph_objs as go
 from nltk.corpus import stopwords
 import pandas as pd
-import numpy as np
 import plotly.plotly as py
+from sklearn.preprocessing import LabelEncoder
 from plotly.figure_factory import create_table
 from plotly.graph_objs import Scatter, Figure, Layout
 import scipy
@@ -14,52 +14,58 @@ import json
 import csv
 from nltk.tokenize import word_tokenize
 import nltk
+import matplotlib.pyplot as plt
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import pandas_datareader.data as web
+import seaborn as sns
+from datetime import datetime
 
-#start and end dates of time period
+#start and end dates of time period for our TSLA stock
+
 start = datetime(2012,11,20)
 end = datetime(2017,9,29)
+
 # Tesla ticker
 ticker='TSLA'
-#we read the data of ticker from yahoo/finance
-data=web.DataReader(ticker, 'yahoo', start, end)
-#print(data)
 
-#the data can be downloaded from the csv. Since we will somewhat use the dates I 
-#used csv to get dates as the column
+#we read the data of ticker from ...yahoo/finance
+data=web.DataReader(ticker, 'yahoo', start, end)
+
+#or
+
+#the data can be downloaded from the .csv file
 data=pd.read_csv(r'/Users/alyonarodin/Desktop/python_em_tweets/TSLA.csv')
+print(data.head())
 #percent change calculated using Adj Close (the Close prices after amendments)
-percent_change=data['Adj Close'].pct_change()*100
+percent_change=data['Adj Close'].pct_change()
 volume=data['Volume']
-#we took two columns of the data downloaded from the yahoo website
-#print(percent_change[1:])
-#print(volume[1:])
 
 #calculated the column(variable) of direction of the return Up and Down
 list_up_down=[]
 n=0
 for value in percent_change:
-        #print(value)
         if (value>0.0):
            list_up_down.append('Up')
            n+=1
         else:
            list_up_down.append('Down')
            n+=1
-
-#print(list_up_down) 
-#print(data['Date'][1:])  
-#created a table : Date, Adj Close, Return, Volume, Direction. Adj Close and
-#Return will be highly correlated, but included... 
-stock_data=pd.DataFrame(list(zip(data['Date'][1:],data['Adj Close'][1:],percent_change[1:],volume[1:],list_up_down)), columns=['date','AdjClose','percentage_change', 'volumne', 'Direction'])
+ 
+#created a table : Date, Adj Close, Return, Volume, Direction 
+stock_data=pd.DataFrame(list(zip(data['Date'],data['Adj Close'],percent_change,volume,list_up_down)), columns=['date','AdjClose','percentage_change', 'volumne', 'Direction'])
+print(stock_data.head())
+print(stock_data.tail())
 #print('Maximum percent change: ', format(percent_change.max()*100, '.2f'),'%')
-#print(stock_data)
 
-#transfered to .json format to be able to use it in the Tableau
+#transfered to .json format to be able to use it in the Tableau to analyse 
 stock_Data_json=stock_data.to_json(orient='table')
-outfile = open("/Users/alyonarodin/Desktop/python_em_tweets/stock_Data_json.json", "w")
-outfile.write(stock_Data_json)
-outfile.close()
+orig_stock_data=data.to_json(orient='table')
+outfile_1 = open("/Users/alyonarodin/Desktop/python_em_tweets/stock_Data_json.json", "w")
+outfile_2 = open("/Users/alyonarodin/Desktop/python_em_tweets/orig_stock_data.json", "w")
+outfile_1.write(stock_Data_json)
+outfile_2.write(stock_Data_json)
+outfile_1.close()
+outfile_2.close()
 
 #but we plotted it also in Plotly. First is a Adj close prices and second data is return
 #define the data
@@ -83,28 +89,25 @@ data=[trace_adjClose, trace_perc_change ]
 #plotting 
 fig=(data)
 py.plot(fig,filename='Adj Close prices of Tesla and Return')
-#downloaded data
+
+#downloaded data of Elon Musk Tweets
 data_elonmusk=pd.read_csv('/users/alyonarodin/Desktop/elonmusk.csv', encoding ='latin1')
-#print(data_elonmusk.head())
 
 #drop few columns
 data_elonmusk.drop(labels='row ID', axis=1,inplace=True)
 data_elonmusk.drop(labels='Retweet from', axis=1,inplace=True)
 data_elonmusk.drop(labels='User', axis=1,inplace=True)
-#print(data_elonmusk.head())
 
 #all letters lower case
 data_elonmusk['Tweet']=data_elonmusk['Tweet'].apply(lambda x:" ".join(x.lower() for x in x.split()))
-print(data_elonmusk['Tweet'].head())
 
 #removed the signs
 data_elonmusk['Tweet']=data_elonmusk['Tweet'].str.replace('[^\w\s]','')
-print(data_elonmusk['Tweet'].head())
 
 #removed stop words 
 stop=stopwords.words('english')
 data_elonmusk['Tweet']=data_elonmusk['Tweet'].apply(lambda x: " ".join(x for x in x.split() if x not in stop))
-print(data_elonmusk['Tweet'].head())
+
 
 # we defined each tweet as negative, positive and neutral
 tweets=[tweet for tweet in data_elonmusk['Tweet']]    
@@ -544,11 +547,110 @@ print(DF_list_tweets_new_table)
 d=DF_list_tweets_new_table[DF_list_tweets_new_table.list_adjCl!= 'NaN']
 #print(len(d))
 
-#next we perform analyses for the table just has been created
+#transfered this to json 
+table_tweets=d.to_json(orient='table')
+
+outfile = open("/Users/alyonarodin/Desktop/python_em_tweets/table_tweets.json", "w")
+
+outfile.write(table_tweets)
+outfile.close() 
+
+#1.looking at the data we can notice that we have categorical and numerical data
+#actually we will look for direction which is also categorical and binary.
+#for that reason for our model we will use logit algorithm, SVM as 
+#its for classificaiton (supervised algorithm)
+
+#we remove the dates
+new_table=d.drop(['time'],axis=1)
+print(new_table)
+
+#since we predict direction UP/DOWN we need to convert it
+#but first we plot
+new_table['list_direction'].value_counts()
+sns.countplot(x='list_direction', data=new_table, palette="coolwarm")
+plt.show()
+#we have approximately the same amount of ups and downs
+#converted Direction values 'Up' and 'Down' to 0 and 1
+N_Direction = LabelEncoder()
+new_table['list_direction_code'] = N_Direction.fit_transform(new_table['list_direction'])
+new_table[["list_direction", "list_direction_code"]].head()
+new_table=new_table.drop(['list_direction'],axis=1)
+print(new_table.head())
+#next we look at the pos/neg/neu
+#its a categorical data and we can seperate it into dummy variables
+#where pos:1, neu:0, neg=-1
+#as well we create dummy variables for columns rt, tesla, spacex,
+#model
+dummy_Pos_Neg=pd.get_dummies(new_table['ps/neg/neu'],prefix='ps/neg/neu')
+dummy_rt=pd.get_dummies(new_table['rt'],prefix='rt')
+dummy_tesla=pd.get_dummies(new_table['tesla'], prefix='tesla')
+dummy_spacex=pd.get_dummies(new_table['spacex'],prefix='spacex')
+dummy_model=pd.get_dummies(new_table['model'],prefix='model')
+new_data_w_dummy=pd.concat([dummy_Pos_Neg,dummy_rt,dummy_tesla,dummy_spacex,dummy_model,new_table['list_adjCl'],new_table['list_return'],new_table['list_volume']], axis=1)
+print(new_data_w_dummy)
+#as we created dummy variables, we also have now multicolinearity. 
+#we need to drop one column in each dummy category for that reason
+#lets drop them
+
+new_data_w_dummy_=new_data_w_dummy.drop(['ps/neg/neu_neg'],axis=1)
+new_data_w_dummy_=new_data_w_dummy_.drop(['rt_0'],axis=1)
+new_data_w_dummy_=new_data_w_dummy_.drop(['tesla_0'],axis=1)
+new_data_w_dummy_=new_data_w_dummy_.drop(['spacex_0'],axis=1)
+new_data_w_dummy_=new_data_w_dummy_.drop(['model_0'],axis=1)
+print(new_data_w_dummy_)
+#now we look at the columns list_adjCl, list_return, and list_volume
+#here we have two columns between which we have correlation 
+#(list_return and list_adj)lets plot it 
+#but first we noticed also that list volume has huge numbers
+#comparing to the rest two columns
+#we need to normalize it 
+#to do it we will use the formula of normalization (x-xmin)/(xmax-xmin)
+#and also explore it a little bit
+#we will plot box plots for this data
+#and histograms
 
 
+def norm(data):
+    new_list=[(i-min(list(data)))/(max(list(data))-min(list(data))) for i in list(data)]    
+    return(new_list)
 
+def absol(data)    :
+    new_list=[abs(i) for i in list(data) ]
+    return(new_list)
 
+if __name__ == '__main__':  
+    new_data_w_dummy_adjCl = norm(new_data_w_dummy_['list_adjCl'])
+    new_data_w_dummy_volume = norm(new_data_w_dummy_['list_volume'])
+    new_data_w_dummy_return = absol(new_data_w_dummy_['list_return'])
+    print(new_data_w_dummy_adjCl[1:4])
+    print(new_data_w_dummy_volume[1:4])
+    print(new_data_w_dummy_return[1:4])
+    
+    
+new_data_w_dummy_ =new_data_w_dummy_.drop(['list_adjCl'],axis=1)   
+new_data_w_dummy_ =new_data_w_dummy_.drop(['list_volume'],axis=1) 
+list_with_norm_data=pd.DataFrame(list(zip(new_data_w_dummy_adjCl,new_data_w_dummy_volume,new_data_w_dummy_return,new_data_w_dummy_['ps/neg/neu_neu'],new_data_w_dummy_['ps/neg/neu_pos'],new_data_w_dummy_['rt_1'],new_data_w_dummy_['tesla_1'],new_data_w_dummy_['spacex_1'],new_data_w_dummy_['model_1'],new_data_w_dummy_['list_direction_code'])), columns=['list_adjCl','list_volume','list_return','ps/neg/neu_neu','pos/neg/neu_pos','rt','tesla','spacex','model','list_direction'])
+print(list_with_norm_data)
+sns.heatmap(list_with_norm_data.corr())
+corr =list_with_norm_data.corr()
+print(corr)    
+
+#preparing data we dropped the rows that have NAN values but
+#matching the dates but it has its disadvantages:
+#1.we can end up removing too much data - actually this is what we had 
+#but we removed a lot of data from the table that had tweets
+#since Elon Musk tweeted sometimes 10 times per day.
+#2. we can lose valuable information-true for us too
+#because we might removed the tweets that effected the direction
+#if he tweeted over the weekend
+#but we did not removed any values from our stock data
+#we will lieave as we have and look what we get.
+#however there is a different approch to deal with missing data
+#-interpolation technique - imputation-replace the missing value by the mean 
+#value of the entire feature column 
+#for the future we can make it and to see the difference
+#(in the second part of report)
+# for the categorical data we can use - most_frequent
 
 
 
