4fc4c5b9424f154349ea7c6590e2a868b29104d1
Alyona Rodin <32145723+TalRodin@users.noreply.github.com>
2018-08-11 23:03:29 -0400

    Add files via upload

--- elm_incr_data.py
+++ elm_incr_data.py
@@ -3,8 +3,11 @@ from sklearn.preprocessing import Imputer
 from array import *
 import statistics
 import pandas as pd
+import numpy as np
+import random
+from sklearn.utils import shuffle
 
-DF_list_tweets_new_table
+print(DF_list_tweets_new_table)
 
 list_d=[]
 for i in DF_list_tweets_new_table['list_direction']:
@@ -74,7 +77,73 @@ for i in DF_list_tweets_new_table['list_direction']:
 print(count_up)
 print(count_down)   
 
-#ups more then downs --> we need increase data of downs
+#ups more then downs --> we need increase data of downs by 2 
+
+def add_data(x):
+ list_=[]
+ if count_up > count_down:
+    for i in range(len(DF_list_tweets_new_table['list_direction'])):
+        if DF_list_tweets_new_table['list_direction'][i]==0:
+
+            list_.append(x[i])
+        
+ return(list_)
+
+pos_neg_neu=add_data(DF_list_tweets_new_table['pos/neg/neu'])
+tweets=add_data(DF_list_tweets_new_table['#tweets'])
+time=add_data(DF_list_tweets_new_table['time'])
+rt=add_data(DF_list_tweets_new_table['rt'])
+tesla=add_data(DF_list_tweets_new_table['tesla'])
+spacex=add_data(DF_list_tweets_new_table['spacex'])
+model=add_data(DF_list_tweets_new_table['model'])
+list_adjCl=add_data(DF_list_tweets_new_table['list_adjCl'])
+list_volume=add_data(DF_list_tweets_new_table['list_volume'])
+list_direction=add_data(DF_list_tweets_new_table['list_direction'])
+
+
+df_down_data=pd.DataFrame(list(zip(pos_neg_neu,tweets,time,rt,tesla,spacex,model,list_adjCl,list_volume,list_direction)),columns=['pos/neg/neu','#tweets','time','rt','tesla','spacex','model','list_adjCl','list_volume','list_direction'])
+print(df_down_data)
+   
+
+
+n_data=pd.concat([DF_list_tweets_new_table, df_down_data])
+print(n_data)
+
+n_data=shuffle(n_data)
+print(n_data.head())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     
